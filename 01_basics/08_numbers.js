bf073feeda806bc5c813468f7b809c6a1c1ea12f
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-06 21:50:22 +0530

    numbers done

---
+++ 01_basics/08_numbers.js
@@ -0,0 +1,32 @@
+const score = 123;
+// console.log(score, typeof score);
+
+let balance = new Number(42000000);
+// console.log(balance, typeof balance);
+
+// console.log(balance.toString(), typeof balance.toString());
+// console.log(balance.toString().length);
+balance = 100;
+// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(2));
+balance = 123.456;
+// console.log(balance.toFixed(1));
+// console.log(balance.toFixed(2));
+
+const randomNumber = 123.456;
+// console.log(randomNumber.toPrecision(3));
+// console.log(randomNumber.toPrecision(4));
+// console.log(randomNumber.toPrecision(2));
+
+const money = 1000000;
+// console.log(money.toLocaleString("en-US"));
+// console.log(money.toLocaleString("en-IN"));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.EPSILON);
+// console.log(Number.NaN);
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(Number.NEGATIVE_INFINITY);
