858e23669a1275104d0e7d7d3fffe944f74fcad8
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-07 12:25:35 +0530

    dates done

---
+++ 01_basics/10_dates.js
@@ -0,0 +1,37 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+let myCreatedDate = new Date(2025, 0, 23, 19, 30);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+myCreatedDate = new Date("2025-01-23");
+myCreatedDate = new Date("01-23-2025");
+// console.log(myCreatedDate.toLocaleDateString());
+// console.log(myCreatedDate.toDateString());
+
+let timeStamp = Date.now();
+// console.log(timeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date(2025, 0, 23, 19, 30);
+// console.log(newDate.toLocaleString());
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getTime());
