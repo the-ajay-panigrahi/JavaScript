4668641bf62973f6c4b3692a916650f6413ae956
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-09 22:47:38 +0530

    some more functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -51,3 +51,44 @@ function loginUserMessage(userName = "Ram") {
 
 // loginUserMessage();
 // loginUserMessage("Ajay");
+
+// The "..." syntax is used in JavaScript.
+// Depending on the context, it's called the rest or spread operator.
+// In this case, we will call "..." as rest operator.
+
+function calculateCartPrice(value1, value2, ...remainingPart) {
+  console.log(typeof remainingPart);
+  return remainingPart;
+}
+
+// console.log(calculateCartPrice(200, 954, 600, 13245, 10, 4933));
+
+const userObject = {
+  userName: "Ajay",
+  age: 20,
+  isLoggedIn: true,
+  status: "offline",
+};
+
+function handleObject(anyObject) {
+  console.log(
+    `The user's name is ${anyObject.userName} and his age is ${anyObject.age}. He's loggedin status is ${anyObject.isLoggedIn} and he is ${anyObject.status}.`
+  );
+}
+
+// handleObject(userObject);
+// handleObject({
+//   userName: "Arjun",
+//   age: 37,
+//   isLoggedIn: false,
+//   status: "N/A",
+// });
+
+const myArray = [100, "got it - 1", 300, 400, 500, 600, 700];
+function returnArraySecondValue(thisArray) {
+  return thisArray[1];
+}
+// console.log(returnArraySecondValue(myArray));
+console.log(
+  returnArraySecondValue([100, "got it - 2", 300, 400, 500, 600, 700])
+);
