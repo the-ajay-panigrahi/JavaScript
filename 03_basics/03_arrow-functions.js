dcf616af51e34f29f06d3f825f8b28b92f453310
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-10 11:37:52 +0530

    arrow functions done

--- 03_basics/03_arrow-functions.js
+++ 03_basics/03_arrow-functions.js
@@ -1,5 +1,5 @@
 // "this" keyword refers to the current context (in JavaScript)
-
+// this keyword refers to an object.
 const user = {
   userName: "Ajay",
   age: 20,
@@ -14,3 +14,33 @@ const user = {
 
 // browser's global object => window
 // console.log(this); // Node.js's global object => {}
+
+function sample() {
+  let userName = "Ajay";
+  console.log(this);
+  console.log(this.userName);
+}
+// sample();
+
+// const value = function random() {
+//   return 1;
+// };
+
+const value = () => {
+  return 1;
+};
+// console.log(value());
+
+// const addTwoNumber = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const addTwoNumber = (num1, num2) => num1 + num2;
+
+// const addTwoNumber = (num1, num2) => (num1 + num2); // heavily used in React.js
+
+// const addTwoNumber = (num1, num2) => { user: "Ajay", age: 20 }; // wrong
+
+const addTwoNumber = (num1, num2) => ({ user: "Ajay", age: 20 });
+
+console.log(addTwoNumber(2, 5));
