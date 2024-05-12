31e8e5cf2ea739ba66ac5a446abc5bc2406fa134
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-12 20:38:54 +0530

    nullish coalescing and ternary operator done

--- 04_control_flow/03_truthy-falsy.js
+++ 04_control_flow/03_truthy-falsy.js
@@ -25,14 +25,33 @@ if (Object.keys(object).length > 0) {
   console.log(`Empty object {}`);
 }
 
-console.log(false == 0);
-console.log(false === 0);
-console.log("\n");
+// console.log(false == 0);
+// console.log(false === 0);
+// console.log("\n");
 
-console.log(false == "");
-console.log(false === "");
-console.log("\n");
+// console.log(false == "");
+// console.log(false === "");
+// console.log("\n");
 
-console.log(0 == "");
-console.log(0 === "");
-console.log("\n");
+// console.log(0 == "");
+// console.log(0 === "");
+// console.log("\n");
+
+/*
+    Nullish Coalescing Operator (??) :- null / undefined
+        => The nullish coalescing (??) operator is a logical operator.
+        => It returns its right-hand side operand when its left-hand side operand is null 
+           or undefined, and otherwise returns its left-hand side operand.
+*/
+
+let val1;
+val1 = 5 ?? 10;
+val1 = null ?? 10;
+val1 = undefined ?? 15;
+val1 = null ?? 10 ?? 20;
+// console.log(val1);
+
+// Ternary Operator
+// condition ? true : false;
+let age = 20;
+age >= 18 ? console.log("Adult") : console.log("Child");
