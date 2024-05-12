4ef4f06e3f2c9f8eeae93653c4e45bb244d8de66
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-12 18:54:20 +0530

    switch-case done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,44 @@
+let month = "oct";
+
+switch (month) {
+  case "jan":
+    console.log("Jan-1");
+    break;
+  case "feb":
+    console.log("Feb-2");
+    break;
+  case "march":
+    console.log("March-3");
+    break;
+  case "apr":
+    console.log("Apr-4");
+    break;
+  case "may":
+    console.log("May-5");
+    break;
+  case "jun":
+    console.log("Jun-6");
+    break;
+  case "jul":
+    console.log("Jul-7");
+    break;
+  case "aug":
+    console.log("Aug-8");
+    break;
+  case "sep":
+    console.log("Sep-9");
+    break;
+  case "oct":
+    console.log("Oct-10");
+    break;
+  case "nov":
+    console.log("Nov-11");
+    break;
+  case "dec":
+    console.log("Dec-12");
+    break;
+
+  default:
+    console.log("default case matched");
+    break;
+}
