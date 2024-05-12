e6921dc084e39c07f61b7e320b7f49beb1c6111d
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-12 22:37:24 +0530

    while loops done

---
+++ 05_iterations/02_while-loops.js
@@ -0,0 +1,14 @@
+// while loops
+let value = 1;
+while (value <= 10) {
+  console.log(value);
+  value = value + 1;
+}
+
+const randomArray = ["Ajay", null, true, "Ram", "Hanuman", undefined, 99];
+
+let index = 0;
+while (index < randomArray.length) {
+  console.log(randomArray[index]);
+  index = index + 1;
+}
