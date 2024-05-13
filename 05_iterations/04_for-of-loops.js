25ef35a46894157704922b1fd303b46ef8060cd1
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-13 21:49:53 +0530

    for-of loops done

---
+++ 05_iterations/04_for-of-loops.js
@@ -0,0 +1,10 @@
+// for of loop
+const myArray = [1, 2, 3, 4, 5, 6, 7];
+for (const value of myArray) {
+  //   console.log(value);
+}
+
+const greetings = "Hello Ajay";
+for (const greet of greetings) {
+  console.log(`Each character is ${greet}`);
+}
