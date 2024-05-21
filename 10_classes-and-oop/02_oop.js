b46193c47f61c0f2c3b623d402540489646fd5fc
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-05-21 15:21:06 +0530

    oops basics

--- 10_classes-and-oop/02_oop.js
+++ 10_classes-and-oop/02_oop.js
@@ -14,3 +14,33 @@ const user = {
 user.getUserDetails();
 
 // console.log(this);
+
+function allUser(userHeight, userWeight, userStatus) {
+  this.userHeight = userHeight;
+  this.userWeight = userWeight;
+  this.userStatus = userStatus;
+
+  this.greeting = function () {
+    console.log(`User's statud: ${this.userStatus}`);
+  };
+
+  return this;
+}
+
+const userOne = new allUser(170, 80, "Online");
+const userTwo = new allUser(173, 65, "Offline");
+// console.log(userOne);
+// console.log(userTwo);
+
+// console.log(userOne.constructor);
+
+console.log(userOne instanceof allUser);
+console.log(userOne instanceof Object);
+
+/*
+new keyword:
+ - whenever you use new keyword an empty object is created which is called instance.
+ - an constructor function is called due to new keyword internally.
+ - this keyword injects all the arguments in these.
+ - and finally we get all the values inside the object
+ */
